43cf186c4154281cd041e388ccb2e4cafbc2fced
mschweiz <marius.schweizer@gmail.com>
2013-05-17 22:43:51 +0200

    c

--- XnaHotWire/XnaHotWire/XnaHotWire/CalibrationScreen.cs
+++ XnaHotWire/XnaHotWire/XnaHotWire/CalibrationScreen.cs
@@ -28,18 +28,18 @@ namespace XnaHotWire
 
         public override void Draw(GameTime gameTime)
         {
-            SpriteBatch.DrawString(Parent.DefaultFont, "Calibrate Controller to zero", new Vector2(100, 50), Color.Black);
+            SpriteBatch.DrawString(Parent.DefaultFont, "Calibrate Controller to zero", new Vector2(100, 50), Color.White);
 
             float x = Parent.SerialController.GetPositionX();
             float y = Parent.SerialController.GetPositionY();
 
-            SpriteBatch.DrawString(Parent.DefaultFont, "X:" + x, new Vector2(100, 125), Color.Black);
-            SpriteBatch.DrawString(Parent.DefaultFont, "Y:" + y, new Vector2(100, 150), Color.Black);
+            SpriteBatch.DrawString(Parent.DefaultFont, "X:" + x, new Vector2(100, 125), Color.White);
+            SpriteBatch.DrawString(Parent.DefaultFont, "Y:" + y, new Vector2(100, 150), Color.White);
 
             if (Parent.SerialController.IsCalibrated())
             {
-                SpriteBatch.DrawString(Parent.DefaultFont, "Calibrated!" + y, new Vector2(100, 175), Color.Black);
-                
+                SpriteBatch.DrawString(Parent.DefaultFont, "Calibrated!" + y, new Vector2(100, 175), Color.White);
+
                 Parent.GotoScreen(ScreenType.Action);
             }
           
